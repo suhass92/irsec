ace64960d71f57a3a041f6462e09b7bf201c9021
Ravi Buddi <raviteja7@users.noreply.github.com>
2017-04-28 14:31:50 -0400

    osquery_ubuntu_orchestrator

---
+++ osquery/osquery.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1484120AC4E9F8A1A577AEEE97A80C63C9D8B80B
+sudo add-apt-repository "deb [arch=amd64] https://osquery-packages.s3.amazonaws.com/xenial xenial main"
+sudo apt-get update
+sudo apt-get install osquery
+
+curl https://raw.githubusercontent.com/suhass92/irsec/master/osquery/osquery.conf > osquery.conf
+
+sudo cp osquery.conf /etc/osquery/
+
+sudo systemctl start osqueryd
