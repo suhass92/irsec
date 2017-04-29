28ff97c629619eee7db57e19f3b01bd3fd5d72a1
Ravi Buddi <raviteja7@users.noreply.github.com>
2017-04-29 08:32:15 -0400

    random

---
+++ linux/firewall/graylog_mail.sh
@@ -0,0 +1,42 @@
+iptables -F
+
+iptables -P INPUT ACCEPT
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+
+iptables -A INPUT -i lo -j ACCEPT
+
+
+
+
+
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+iptables -A OUTPUT -p tcp --dport 25 -j REJECT
+
+iptables -A INPUT -p tcp --dport 25 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 25 -m state --state ESTABLISHED -j ACCEPT
+
+iptables -A INPUT -p tcp --dport 143 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 143 -m state --state ESTABLISHED -j ACCEPT
+
+iptables -A INPUT -p tcp --dport 993 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 993 -m state --state ESTABLISHED -j ACCEPT
+
+iptables -A INPUT -p tcp --dport 110 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 110 -m state --state ESTABLISHED -j ACCEPT
+
+iptables -A INPUT -p tcp --dport 995 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 995 -m state --state ESTABLISHED -j ACCEPT
+
+iptables -A INPUT -p tcp --dport 9000 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 9000 -m state --state ESTABLISHED -j ACCEPT
+
+ 
+
+iptables -A INPUT -j DROP
+iptables -A OUTPUT -j DROP
+
+/sbin/service iptables save
+ 
+ 
