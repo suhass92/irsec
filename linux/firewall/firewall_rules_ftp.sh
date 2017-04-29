cac23da12e190c8a1793d43432a82c93c8b4e93c
Ravi Buddi <raviteja7@users.noreply.github.com>
2017-04-29 08:14:37 -0400

    changes

--- linux/firewall/firewall_rules_ftp.sh
+++ linux/firewall/firewall_rules_ftp.sh
@@ -8,13 +8,19 @@
 #
 # Allow SSH connections on tcp port 22
 # This is essential when working on remote servers via SSH to prevent locking yourself out of the system
+
+
+ iptables -A INPUT -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
 #
  iptables -A INPUT -p tcp --dport 20 -j ACCEPT
  iptables -A INPUT -p tcp --dport 21 -j ACCEPT
+ 
+iptables -A OUTPUT -p tcp --dport 3306 --dst X.X.X.X -j ACCEPT	
+iptables -A INPUT -p tcp --sport 3306 --src X.X.X.X -j ACCEPT 
 #
 # Set default policies for INPUT, FORWARD and OUTPUT chains
 #
- iptables -P INPUT DROP
+ iptables -P INPUT ACCEPT
  iptables -P FORWARD DROP
  iptables -P OUTPUT ACCEPT
 #
@@ -25,6 +31,9 @@
 # Accept packets belonging to established and related connections
 #
  iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+ 
+ iptables -A INPUT -j DROP
+ iptables -A OUTPUT -j DROP
 #
 # Save settings
 #
